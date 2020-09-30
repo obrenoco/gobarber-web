5b01a03e587955b9e9082dfd5d18dddc45a416fd
Your Name <bsromeiro@gmail.com>
2020-09-29 21:54:13 -0300

    add: yup validations

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,6 +1,8 @@
 /* eslint-disable object-curly-newline */
-import React from 'react';
+import React, { useCallback } from 'react';
 import { Form } from '@unform/web';
+import * as Yup from 'yup';
+
 import { FiMail, FiLock, FiUser, FiArrowLeft } from 'react-icons/fi';
 import logoImg from '../../assets/logo.svg';
 
@@ -10,9 +12,23 @@ import Button from '../../components/Button';
 import { Container, Content, Background } from './styles';
 
 const SignIn: React.FC = () => {
-  function handleSubmit(data: object): void {
-    console.log(data);
-  }
+  const handleSubmit = useCallback(async (data: object) => {
+    try {
+      const schema = Yup.object().shape({
+        name: Yup.string().required('Nome obrigatório'),
+        email: Yup.string()
+          .required('E-mail obrigatório')
+          .email('Digite um e-mail válido'),
+        password: Yup.string().min(6, 'A senha deve conter 6 ou mais dígitos'),
+      });
+
+      await schema.validate(data, {
+        abortEarly: false,
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  }, []);
 
   return (
     <Container>
