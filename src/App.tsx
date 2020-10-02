c6c34514ca4a3cbcc25066cebe8ea9fd441c4a73
Your Name <bsromeiro@gmail.com>
2020-10-01 23:10:25 -0300

    add: toast with hooks

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,16 @@ import React from 'react';
 import SignIn from './pages/SignIn';
 // import SignUp from './pages/SignUp';
 
-import { AuthProvider } from './hooks/AuthContext';
+import AppProvider from './hooks/index';
 
 import GlobalStyle from './styles/global';
 
 const App: React.FC = () => (
   <>
-    <AuthProvider>
+    <AppProvider>
       <SignIn />
-    </AuthProvider>
+    </AppProvider>
+
     {/* <SignUp /> */}
     <GlobalStyle />
   </>
